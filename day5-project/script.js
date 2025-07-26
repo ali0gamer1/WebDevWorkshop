78a3202c6957048c9762b660798b8463e5ccd5d7
Ali <gta7iv7@gmail.com>
2025-07-26 18:23:36 +0330

    heu

--- day5-project/script.js
+++ day5-project/script.js
@@ -6,6 +6,34 @@ let paddingElement = document.getElementById("padding-controller-field");
 let theBigBox = document.getElementById("TheBiggerOne");
 
 
+function changeCords(posVal, whichPos, whichBox)
+{
+    theBox = null;
+
+    if (whichBox == 1)
+        theBox = theBigBox;
+    else
+        theBox = theSmallBox;
+    
+    theBox.style[whichPos] = `${posVal}px`;
+    
+}
+
+function changeDisplay(test, whichBox = 1)
+{
+    theBox = null;
+
+    if (whichBox == 1)
+        theBox = theBigBox;
+    else
+        theBox = theSmallBox;
+    
+
+    const posVals = ["static", "relative"];
+
+    theBox.style.position = posVals[+test.checked];
+
+}
 
 
 function changePaddingValue(paddingVal, whichBox = 1)
