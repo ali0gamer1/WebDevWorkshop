265c9538c470faa74a1aeabba9554009dc2f083a
ali0gamer1 <gta7iv7@gmail.com>
2025-07-27 12:14:04 +0330

    fixed menu + clear button

--- day5-project/script.js
+++ day5-project/script.js
@@ -4,6 +4,75 @@
 let theSmallBox = document.getElementById("TheSmallerOne");
 let paddingElement = document.getElementById("padding-controller-field");
 let theBigBox = document.getElementById("TheBiggerOne");
+const menu = document.getElementById('draggable-menu');
+
+  
+// Dragging logic
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
+let isMenu = false;
+
+
+
+function clearInputs()
+{
+
+  theSmallBox.style = "";
+  theBigBox.style = "";
+
+  inputs = Array.from(document.getElementsByTagName("input"));
+  inputs.forEach(element => {
+
+    if (element.type == "checkbox")
+      {
+        element.checked = false;  
+        return;
+      }
+    
+   
+      element.value = 0
+      
+      element.previousElementSibling.value = 0;
+      
+      if (element.id == "bigBox")
+      {
+        element.value = 25;
+        element.previousElementSibling.value = 25;
+      }   
+      
+      
+  });
+
+
+  
+}
+
+
+menu.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  if (e.target === menu)
+    isMenu = true;
+  offsetX = e.clientX - menu.offsetLeft;
+  offsetY = e.clientY - menu.offsetTop;
+  document.body.style.userSelect = "none"; // prevent selecting text while dragging
+});
+
+
+document.addEventListener('mousemove', (e) => {
+  if (isDragging && isMenu) {
+    menu.style.left = (e.clientX - offsetX) + 'px';
+    menu.style.top = (e.clientY - offsetY) + 'px';
+  }
+});
+
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+  isMenu = false;
+  document.body.style.userSelect = ""; // restore text selection
+});
+
 
 
 function changeCords(posVal, whichPos, whichBox)
